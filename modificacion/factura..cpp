9c9141b5c23d114032fd34f709af8aaa16d5de84
denismeza7 <106563202+denismeza7@users.noreply.github.com>
2022-07-13 14:56:44 -0600

    Add files via upload

---
+++ modificacion/factura..cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal;
+float impuesto = 15;
+double totalAPagar;
+string listaProductos;
+	
+	void agregarProducto(string descripcion,int cantidad,double precio) 
+    { 
+        listaProductos = listaProductos + descripcion + "\n";
+        subtotal = subtotal + (cantidad * precio);
+	}	
+	
+	void imprimirFactura()
+	{
+		system("cls");
+		cout << "*******" << endl;
+		cout << "Factura" << endl;
+		cout << "*******" << endl;
+		cout << endl;
+		
+		cout << "Productos: " << endl;
+		cout << listaProductos;
+		
+		cout << endl;
+		impuesto = impuesto / 100;
+		impuesto = subtotal * impuesto;
+		totalAPagar = subtotal + impuesto;
+		cout << "Subtotal: " << subtotal;
+		cout << endl;
+		cout << "Total Pago: " <<totalAPagar << endl;
+		cout << endl;
+		system("pause");	
+	}
---
+++ modificacion/main..cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "menu.h"
+using namespace std;
+int main()
+{
+	menu();
+	
+	return 0;
+	
+}
