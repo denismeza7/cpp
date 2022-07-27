81f65bc695450cd58282ad9d5210f0e177d8eb4d
denismeza7 <106563202+denismeza7@users.noreply.github.com>
2022-07-27 17:08:37 -0600

    Add files via upload

---
+++ Funciones con Ciclos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimirCaracteres(char caracter, int ciclos)
+{
+	for (int i = 0; i < ciclos; i++)
+	{
+		cout << caracter;
+	}
+	
+	cout << endl;
+}
+
+int main(int argc, char const *argv[]) 
+{
+	imprimirCaracteres('@', 10);
+	imprimirCaracteres('#', 15);
+	cout << "Hola Mundo" << endl;
+	imprimirCaracteres('*', 100);
+	return 0;
+}
